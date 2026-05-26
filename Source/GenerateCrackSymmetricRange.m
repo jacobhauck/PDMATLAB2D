1f32f96b9170d06ea801a270a3efea3146d1c566
Jacob Hauck <jphqkc@umsystem.edu>
2026-05-26 12:37:06 -0500

    Add symmetric range

---
+++ Source/GenerateCrackSymmetricRange.m
@@ -0,0 +1,85 @@
+sim = Simulation();
+
+sim.flag_DynamicPlotting = false;
+sim.flag_video = false;
+sim.flag_ShowProgress = false;
+
+zero_t = @(x, y, t) 0.0*x;
+sim.bvfunc = zero_t;
+
+sim.Ti = 0.0;
+sim.Tf = 4.3e-5; % [s] :    43 microsec
+sim.dt = 6.7E-8; % [s] : 67E-3 microsec
+sim.del = 0.001;
+sim.flag_BB = true;
+sim.rho = 2440; % [kg/m^3]
+sim.E = 72e+9;  % [Pa] : 72 GPa 
+sim.Go = 3.8;   % [J/m^2]
+
+Xo = -0.05; % [m] : Left  boundary of the domain 
+Xn =  0.05; % [m] : Right boundary of the domain
+Yo = -0.02; % [m] : Lower boundary of the domain
+Yn =  0.02; % [m] : Upper boundary of the domain
+
+Nx = 300;
+Ny = 120;
+dy = (Yn - Yo) / Ny;
+
+sim.LoadGrid("GridFile2.mat");
+PreNotchCoordinates = [-0.05  0.0  0.0  0.0];
+sim.CreatePreNotches(PreNotchCoordinates, true);
+
+Nbx = 33;
+bx = linspace(Xo, Xn, Nbx)';  % (Nbx, 1)
+
+sim.mask_nofail = (abs(sim.yy) > Yn - sim.del); 
+
+sim.ComputePDConstants();
+
+numChunks = 32;
+datasetSize = 128;
+seed = 1234;
+
+sigma = 2E6;
+sigmaRange = [0.45, 0.6];
+
+datasetName = "sym.ol.h5";
+
+xy = [sim.xx, sim.yy];
+generator = MakeGenerator(datasetSize, numChunks, sigmaRange, Yo, Yn, dy, sigma, bx, PreNotchCoordinates);
+GenerateDataset(datasetName, datasetSize, numChunks, seed, sim, bx, xy, 2, 1, generator);
+
+function generator = MakeGenerator(datasetSize, numChunks, sigmaRange, Yo, Yn, dy, sigma, bx, PreNotchCoordinates)
+    function GenerateOne(simulation, outputFile, chunkIndex, sampleIndex, ~)
+        minChunkSize = floor(datasetSize / numChunks);
+        numExtras = mod(datasetSize, numChunks);
+        globalIndex = (chunkIndex - 1) * minChunkSize + min(numExtras, chunkIndex - 1) + sampleIndex;
+        t = (globalIndex - 1) / (datasetSize - 1);
+        sigmaRel = t * (sigmaRange(2) - sigmaRange(1)) + sigmaRange(1);
+        modtop = @(x) ones(size(x)) * sigmaRel;
+        modbot = @(x) ones(size(x)) * sigmaRel;
+        
+        % y-component of body force density
+        simulation.bwfunc = @(x,y,t) ( (y > Yn - dy) .* modtop(x) + (y < Yo + dy) .* modbot(x) ).*sigma.*sign(y)/dy;
+        
+        % Reset simulation state (need to call LoadGrid again to reset broken
+        % bonds)
+        zero = @(x, y) 0.0 * x;
+        simulation.ImposeInitialConditions(zero, zero, zero, zero);
+        simulation.LoadGrid("GridFile2.mat");
+        simulation.CreatePreNotches(PreNotchCoordinates, true);
+    
+        % Get boundary modulators
+        u = [modtop(bx), modbot(bx)];  % (Nbx, 2)
+        
+        % Solve
+        simulation.Solve();
+    
+        % Get final damage field
+        v = simulation.ComputeDamage();  % (numNodes, 1)
+        
+        WriteOLPDSample(outputFile, sampleIndex, u, v);
+    end
+
+    generator = @GenerateOne;
+end
